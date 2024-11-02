89975f83b00f3fe91bcce1c48763c4bf578416d1
Sunil Vishwakarma <sunil.vis2051991@gmail.com>
2024-11-02 16:16:19 +0530

    remove log file

--- application/logs/log-2024-11-02.php
+++
@@ -1,8 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2024-11-02 11:27:16 --> 404 Page Not Found: ../modules/pwfpanel/controllers/Pwfpanel/img
-ERROR - 2024-11-02 11:31:31 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\x-test-hospital.ioready.io\application\modules\reportsSummary\views\dashboards.php 222
-ERROR - 2024-11-02 11:31:31 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\x-test-hospital.ioready.io\application\modules\reportsSummary\views\dashboards.php 479
-ERROR - 2024-11-02 11:31:31 --> Severity: Warning --> A non-numeric value encountered C:\xampp\htdocs\x-test-hospital.ioready.io\application\modules\reportsSummary\controllers\ReportsSummary.php 3197
-ERROR - 2024-11-02 11:31:31 --> Severity: Warning --> Division by zero C:\xampp\htdocs\x-test-hospital.ioready.io\application\modules\reportsSummary\controllers\ReportsSummary.php 3197
-ERROR - 2024-11-02 11:31:31 --> Severity: Warning --> Division by zero C:\xampp\htdocs\x-test-hospital.ioready.io\application\modules\reportsSummary\controllers\ReportsSummary.php 3197
