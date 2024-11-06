5bcf8ebac2d3074024d6fac545c845ab83f589b2
Sunil Vishwakarma <sunil.vis2051991@gmail.com>
2024-11-06 11:46:11 +0530

    jjkdskjds

--- application/modules/users/views/managements.php
+++ application/modules/users/views/managements.php
@@ -1,3 +1,6 @@
+<link href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-whatever-integrity-code" crossorigin="anonymous">
+
 <style>
         .tab-content {
             margin-top: 20px;
