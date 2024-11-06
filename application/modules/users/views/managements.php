12cb126f906564f433089f763f77315a952894a0
Sunil Vishwakarma <sunil.vis2051991@gmail.com>
2024-11-06 11:40:48 +0530

    change css datain user

--- application/modules/users/views/managements.php
+++ application/modules/users/views/managements.php
@@ -1,6 +1,4 @@
-<link href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css" rel="stylesheet">
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-whatever-integrity-code" crossorigin="anonymous">
+<link href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css" rel="stylesheet"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-whatever-integrity-code" crossorigin="anonymous">
 
 
     <style>
