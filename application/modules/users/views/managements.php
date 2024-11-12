e10ba6b0261aae9ee39dfacbd8d85b44fad0614d
Sunil Vishwakarma <sunil.vis2051991@gmail.com>
2024-11-12 20:00:32 +0530

    update user management add user

--- application/modules/users/views/managements.php
+++ application/modules/users/views/managements.php
@@ -806,110 +806,112 @@
                                     <h2 class="modal-title" id="addUserModalLabel"><strong> Add User</strong></h2>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
+                                <br>
+                                <br><br><br><br><br><br>
                                 <div class="modal-body">
                             
 
-                            <form class="form-horizontal p-4" role="form" id="addFormAjax" method="post" action="<?php echo base_url('users/users_add') ?>" enctype="multipart/form-data">
-                                
-                                <div class="alert alert-danger" id="error-box" style="display: none"></div>
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <div class="form-group me-2">
-                                                <label class="control-label">First Name</label>
-                                                <input type="text" class="form-control" name="first_name" id="first_name" placeholder="First Name" />
-                                            </div>
-                                        </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group me-2">
-                                            <label class="control-label">Last Name</label>
-                                            <input type="text" class="form-control" name="last_name" id="last_name" placeholder="Last Name" />
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group me-2">
-                                            <label class="control-label"><?php echo lang('user_email');?></label>
-                                            <input type="email" class="form-control" name="user_email" id="user_email" placeholder="<?php echo lang('user_email');?>" />
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group me-2">
-                                            <label class="control-label">Country</label>
-                                            <select id="country" name="country" class="form-control select2" size="1">
-                                                <option value="" disabled selected>Please select</option>
-                                                <?php foreach($countries as $country){?>
-                                                <option value="<?php echo $country->id;?>"><?php echo $country->name;?></option>
-                                                <?php }?>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group me-2">
-                                            <label class="control-label"><?php echo lang('phone_no');?></label>
-                                            <input type="text" class="form-control" name="phone_no" id="phone_no" placeholder="<?php echo lang('phone_no');?>" />
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group me-2">
-                                            <label class="control-label"><?php echo lang('password');?></label>
-                                            <input type="text" class="form-control" name="password" id="password" placeholder="<?php echo lang('password');?>" value="<?php echo randomPassword();?>" />
-                                        </div>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label for="role">Role</label>
-                                            <!-- <select class="form-select" id="role" name="role">
-                                                
-                                            <?php //foreach ($roles_list as $rows): ?>
-                                                    <option value="<?php //echo $rows->id; ?>"><?php echo $rows->name; ?></option>
-                                                                
-                                                <?php //endforeach?>
-                                            </select> -->
-
-                                            <select class="form-select" id="role" name="role">
-                                                <?php foreach ($roles_list as $rows): ?>
-                                                    <?php if ($rows->name !== 'Patient' && $rows->name !== 'Doctor' && $rows->name !== 'SubAdmin'): ?>
-                                                        <option value="<?php echo $rows->id; ?>"><?php echo $rows->name; ?></option>
-                                                    <?php endif; ?>
-                                                <?php endforeach; ?>
-                                            </select>
-                                        </div>
-                                    </div>
-
-                                    <div class="col-md-12">
-                                        <div class="form-group me-2">
-                                            <label class="control-label"><?php echo lang('profile_image'); ?></label>
-                                            <div class="group_filed">
-                                                <div class="img_back_prieview_Academic">
-                                                    <div class="images_box_upload_ven_adduser_vendore">
-                                                        <div id="image-preview-adduser-vendore">
-                                                            <input type="file" name="user_image" id="image-upload-adduser-vendore" />
-                                                        </div>
-                                                    </div>
-                                                    <div id="image-preview-adduser">
-                                                        <label for="image-upload-adduser-vendore" id="image-label-adduser-vendore"  class="btn btn-sm btn-primary" style="background:#337ab7;">Upload Logo</label>
+                                    <form class="form-horizontal p-4" role="form" id="addFormAjax" method="post" action="<?php echo base_url('users/users_add') ?>" enctype="multipart/form-data">
+                                        
+                                        <div class="alert alert-danger" id="error-box" style="display: none"></div>
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group me-2">
+                                                        <label class="control-label">First Name</label>
+                                                        <input type="text" class="form-control" name="first_name" id="first_name" placeholder="First Name" />
                                                     </div>
                                                 </div>
+                                            <div class="col-md-6">
+                                                <div class="form-group me-2">
+                                                    <label class="control-label">Last Name</label>
+                                                    <input type="text" class="form-control" name="last_name" id="last_name" placeholder="Last Name" />
+                                                </div>
                                             </div>
-                                            <div class="ceo_file_error file_error text-danger"></div>
-                                        </div>
-                                    </div>
-                                
-                                    <div class="col-md-12">
-                                        <div class="modal-footer">
-                                            <button type="submit" id="submit"  class="btn btn-sm btn-primary" style="background:#337ab7;"><?php echo lang('submit_btn');?></button>
+                                            <div class="col-md-6">
+                                                <div class="form-group me-2">
+                                                    <label class="control-label"><?php echo lang('user_email');?></label>
+                                                    <input type="email" class="form-control" name="user_email" id="user_email" placeholder="<?php echo lang('user_email');?>" />
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-group me-2">
+                                                    <label class="control-label">Country</label>
+                                                    <select id="country" name="country" class="form-control select2" size="1">
+                                                        <option value="" disabled selected>Please select</option>
+                                                        <?php foreach($countries as $country){?>
+                                                        <option value="<?php echo $country->id;?>"><?php echo $country->name;?></option>
+                                                        <?php }?>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-group me-2">
+                                                    <label class="control-label"><?php echo lang('phone_no');?></label>
+                                                    <input type="text" class="form-control" name="phone_no" id="phone_no" placeholder="<?php echo lang('phone_no');?>" />
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-group me-2">
+                                                    <label class="control-label"><?php echo lang('password');?></label>
+                                                    <input type="text" class="form-control" name="password" id="password" placeholder="<?php echo lang('password');?>" value="<?php echo randomPassword();?>" />
+                                                </div>
+                                            </div>
+                                            <div class="col-md-12">
+                                                <div class="form-group">
+                                                    <label for="role">Role</label>
+                                                    <!-- <select class="form-select" id="role" name="role">
+                                                        
+                                                    <?php //foreach ($roles_list as $rows): ?>
+                                                            <option value="<?php //echo $rows->id; ?>"><?php echo $rows->name; ?></option>
+                                                                        
+                                                        <?php //endforeach?>
+                                                    </select> -->
+
+                                                    <select class="form-select" id="role" name="role">
+                                                        <?php foreach ($roles_list as $rows): ?>
+                                                            <?php if ($rows->name !== 'Patient' && $rows->name !== 'Doctor' && $rows->name !== 'SubAdmin'): ?>
+                                                                <option value="<?php echo $rows->id; ?>"><?php echo $rows->name; ?></option>
+                                                            <?php endif; ?>
+                                                        <?php endforeach; ?>
+                                                    </select>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-md-12">
+                                                <div class="form-group me-2">
+                                                    <label class="control-label"><?php echo lang('profile_image'); ?></label>
+                                                    <div class="group_filed">
+                                                        <div class="img_back_prieview_Academic">
+                                                            <div class="images_box_upload_ven_adduser_vendores">
+                                                                <div id="image-preview-adduser-vendore" class="image_vendores">
+                                                                    <input type="file" name="user_image" id="image-upload-adduser-vendore" class="image_vendores"/>
+                                                                </div>
+                                                            </div>
+                                                            <div id="image-preview-adduser">
+                                                                <label for="image-upload-adduser-vendore" id="image-label-adduser-vendore"  class="btn btn-sm btn-primary"  style="background:#337ab7;">Upload Logo</label>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="ceo_file_error file_error text-danger"></div>
+                                                </div>
+                                            </div>
                                         
+                                            <div class="col-md-12">
+                                                <div class="modal-footer">
+                                                    <button type="submit" id="submit"  class="btn btn-sm btn-primary" style="background:#337ab7;"><?php echo lang('submit_btn');?></button>
+                                                
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
+
+                                    </form>
+
                                 </div>
-
-                            </form>
-
                             </div>
-                        </div>
                         
-                    </div>
+                        </div>
 
-                </div>
+                    </div>
 
                 <!-- Add Role Modal (Similar to User) -->
 
@@ -974,6 +976,34 @@
 <div id="form-modal-box"></div>
 
 <div id="form-modal-box-user"></div>
+
+<style>
+    .images_box_upload_ven_adduser_vendores{
+        height: 52px;
+    border: 3px dashed #D0D0D0;
+    padding: 5px;
+    /* margin: 0 auto; */
+    border-radius: 5px;
+    }
+    
+    .image_vendores {
+    width: 42px!important;
+    height: 40px!important;
+    position: relative;
+    overflow: hidden;
+    /* background-color: #ffffff; */
+    color: #ecf0f1;
+    background-image: url(../img/img-upload.png);
+    background-size: cover;
+    background-position: center center;
+    /* border-radius: 50%; */
+    /* float: left; */
+    background-repeat: no-repeat;
+    margin: 0 auto;
+}
+
+</style>
+
 
 <!-- <input type="hidden" id="UserStatus" value="<?php echo $status;?>" /> -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
