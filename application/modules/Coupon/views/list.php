294b52c105c0532da06726b9e7f8678aaaf26a73
shekhar000gurjar <shekhar000gurjar@gmail.com>
2024-11-18 19:32:23 +0530

    update new code

--- application/modules/Coupon/views/list.php
+++ application/modules/Coupon/views/list.php
@@ -170,16 +170,44 @@ display:none;
                                 <td><?php echo $rows->percentage_in_amount; ?></td>
                                 
 
-                                <td class="actions">
-                                    <a href="javascript:void(0)" class="btn btn-xs btn-default" onclick="editFn('<?php echo $model; ?>', 'edit', '<?php echo encoding($rows->id) ?>', '<?php echo $model; ?>');"><i class="fa fa-pencil"></i></a>
-                                    <?php if ($rows->status == 0) { ?>
-                                        <a href="javascript:void(0)" class="btn btn-xs btn-success" onclick="editStatusFn('<?php echo $tablePrefix; ?>', 'id', '<?php echo encoding($rows->id); ?>', '<?php echo $rows->status; ?>','<?php echo $rows->name; ?>')" title="Inactive Now"><i class="fa fa-check"></i></a>
-                                    <?php } else { ?>
-                                        <a href="javascript:void(0)" class="btn btn-xs btn-danger" onclick="editStatusFn('<?php echo $tablePrefix; ?>', 'id', '<?php echo encoding($rows->id); ?>', '<?php echo $rows->status; ?>','<?php echo $rows->name; ?>')" title="Active Now"><i class="fa fa-times"></i></a>
-                                    <?php } ?>
-                                    <a href="javascript:void(0)" data-toggle="tooltip"   onclick="deleteFn('<?php echo $table; ?>', 'id', '<?php echo encoding($rows->id); ?>', 'index.php/Coupon', 'index.php/Coupon/delete','<?php echo $rows->name; ?>')" class="btn btn-danger"><i class="fa fa-trash"></i></a>
-                                    <!-- <a href="javascript:void(0)" onclick="deleteFn('<?php echo $table; ?>', 'id', '<?php echo encoding($rows->id); ?>', '<?php echo $model; ?>','','<?php echo $rows->name; ?>')" class="btn btn-xs btn-danger"><i class="fa fa-trash"></i></a> -->
-                                </td>
+                                <td class="actions" style="display: flex; gap: 10px; align-items: center; justify-content: center;">
+    <!-- Edit Button -->
+    <a href="javascript:void(0)" 
+       class="btn btn-xs btn-default" 
+       style="display: flex; align-items: center; justify-content: center; width: 32px; height: 32px; border-radius: 4px; background-color: #f0f0f0; border: 1px solid #ccc;" 
+       onclick="editFn('<?php echo $model; ?>', 'edit', '<?php echo encoding($rows->id); ?>', '<?php echo $model; ?>');">
+        <i class="fa fa-pencil" style="color: #333;"></i>
+    </a>
+
+    <!-- Status Button -->
+    <?php if ($rows->status == 0) { ?>
+        <a href="javascript:void(0)" 
+           class="btn btn-xs btn-success" 
+           style="display: flex; align-items: center; justify-content: center; width: 32px; height: 32px; border-radius: 4px; background-color: #28a745; border: none;" 
+           onclick="editStatusFn('<?php echo $tablePrefix; ?>', 'id', '<?php echo encoding($rows->id); ?>', '<?php echo $rows->status; ?>', '<?php echo $rows->name; ?>')" 
+           title="Set as Inactive">
+            <i class="fa fa-check" style="color: #fff;"></i>
+        </a>
+    <?php } else { ?>
+        <a href="javascript:void(0)" 
+           class="btn btn-xs btn-danger" 
+           style="display: flex; align-items: center; justify-content: center; width: 32px; height: 32px; border-radius: 4px; background-color: #dc3545; border: none;" 
+           onclick="editStatusFn('<?php echo $tablePrefix; ?>', 'id', '<?php echo encoding($rows->id); ?>', '<?php echo $rows->status; ?>', '<?php echo $rows->name; ?>')" 
+           title="Set as Active">
+            <i class="fa fa-times" style="color: #fff;"></i>
+        </a>
+    <?php } ?>
+
+    <!-- Delete Button -->
+    <a href="javascript:void(0)" 
+       class="btn btn-xs btn-danger" 
+       style="display: flex; align-items: center; justify-content: center; width: 32px; height: 32px; border-radius: 4px; background-color: #dc3545; border: none;" 
+       onclick="deleteFn('<?php echo $table; ?>', 'id', '<?php echo encoding($rows->id); ?>', 'index.php/Coupon', 'index.php/Coupon/delete', '<?php echo $rows->name; ?>')" 
+       title="Delete">
+        <i class="fa fa-trash" style="color: #fff;"></i>
+    </a>
+</td>
+
                             </tr>
                             <?php } }  ?>
                 </tbody>
